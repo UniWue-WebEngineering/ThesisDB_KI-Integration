c4484616d1aa25d0b9ed229499bfb2e67ca2e5f8
fredericthiesse <frederic.thiesse@uni-wuerzburg.de>
2026-04-29 21:54:44 +0200

    Datenmodell inkl. Migrationen

---
+++ ThesisDB/Migrations/20260429171026_FirstMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThesisDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Theses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Theses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Theses");
+        }
+    }
+}
---
+++ ThesisDB/Migrations/20260429184459_AddProgrammeModel.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThesisDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProgrammeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProgrammeId",
+                table: "Theses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Programmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Programmes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Theses_ProgrammeId",
+                table: "Theses",
+                column: "ProgrammeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Theses_Programmes_ProgrammeId",
+                table: "Theses",
+                column: "ProgrammeId",
+                principalTable: "Programmes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Theses_Programmes_ProgrammeId",
+                table: "Theses");
+
+            migrationBuilder.DropTable(
+                name: "Programmes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Theses_ProgrammeId",
+                table: "Theses");
+
+            migrationBuilder.DropColumn(
+                name: "ProgrammeId",
+                table: "Theses");
+        }
+    }
+}
