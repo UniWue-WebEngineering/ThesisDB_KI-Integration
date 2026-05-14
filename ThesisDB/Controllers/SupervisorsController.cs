e30d03b3e433790e1a384509c9d035e9bd23d639
fredericthiesse <frederic.thiesse@uni-wuerzburg.de>
2026-05-14 11:03:12 +0200

    SupervisorsController

---
+++ ThesisDB/Controllers/SupervisorsController.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ThesisDB.Models;
+
+namespace ThesisDB.Controllers
+{
+    public class SupervisorsController : Controller
+    {
+        private readonly ThesisDbContext _context;
+
+        public SupervisorsController(ThesisDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Supervisors
+        public async Task<IActionResult> Index()
+        {
+            var supervisors = await _context.Supervisors
+                .Include(s => s.Theses)
+                .OrderBy(s => s.LastName)
+                .ThenBy(s => s.FirstName)
+                .ToListAsync();
+            return View(supervisors);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FirstName,LastName,Email,Active")] Supervisor supervisor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(supervisor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,Active")] Supervisor supervisor)
+        {
+            if (id != supervisor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(supervisor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SupervisorExists(supervisor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var supervisor = await _context.Supervisors
+                .Include(s => s.Theses)
+                .FirstOrDefaultAsync(m => m.Id == id);
+                
+            if (supervisor != null && !supervisor.Theses.Any())
+            {
+                _context.Supervisors.Remove(supervisor);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SupervisorExists(int id)
+        {
+            return _context.Supervisors.Any(e => e.Id == id);
+        }
+    }
+}
