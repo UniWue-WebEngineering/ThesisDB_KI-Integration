ce56542b7ddba9f7cbfa4d9a789d680909a8f596
fredericthiesse <frederic.thiesse@uni-wuerzburg.de>
2026-05-31 23:05:48 +0200

    PdfUpload Migration

---
+++ ThesisDB/Migrations/20260531201129_PdfUpload.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThesisDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class PdfUpload : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PdfFileName",
+                table: "Theses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PdfFileName",
+                table: "Theses");
+        }
+    }
+}
